33ba1d356f56d0b51debfbf36fd665b62691af41
Javohirbek <javohirbekkhaydarov@gmail.com>
2022-09-21 23:47:57 +0500

    complete about section

---
+++ elearning/src/components/AboutSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import reading from "../img/reading.jpg";
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2> Be educated so that</h2>
+          </div>
+          <div className="hide">
+            <h2> you can change</h2>
+          </div>
+          <div className="hide">
+            <h2> world.</h2>
+          </div>
+          <p>
+            {" "}
+            An educated mind can teach many. And educated mind is better than
+            empty
+          </p>
+          <button> About Us</button>
+        </div>
+      </div>
+      <div className="image">
+        <img src={reading} alt="school library man" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
