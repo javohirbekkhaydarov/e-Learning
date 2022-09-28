a7cc570db0e7c56021762d9f3be35e2401a834c8
Javohirbek <javohirbekkhaydarov@gmail.com>
2022-09-28 23:19:00 +0500

    frame

--- elearning/src/components/AboutSection.js
+++ elearning/src/components/AboutSection.js
@@ -1,34 +1,73 @@
 import React from "react";
 import reading from "../img/reading.jpg";
-import styled from "styled-components";
 import { About, Description, Hide, Image } from "../styles";
+
+import { motion } from "framer-motion";
 const AboutSection = () => {
   return (
     <About>
       <Description>
         <div className="title">
+
           <Hide>
-            <h2> Be educated so that</h2>
+            <motion.h2
+              animate={{
+                opacity: 1,
+                transform: { translateX: 0 },
+                transition: { duration: 0.5 },
+                y: 0,
+              }}
+              initial={{ opacity: 0, y: -40 }}
+            >
+              {" "}
+              Be educated so that
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2
+              animate={{
+                opacity: 1,
+                transform: { translateX: 0 },
+                transition: { duration: 0.7 },
+                y: 0,
+              }}
+              initial={{ opacity: 0, y: -40 }}
+            >
               {" "}
               you <span> can change</span>
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2> world.</h2>
+            <motion.h2
+              animate={{
+                opacity: 1,
+                transform: { translateX: 0 },
+                transition: { duration: 1 },
+                y: 0,
+              }}
+              initial={{ opacity: 0, y: -40 }}
+            >
+              {" "}
+              world.
+            </motion.h2>
           </Hide>
           <p>
             {" "}
             An educated mind can teach many. And educated mind is better than
             empty
           </p>
-          <button> About Us</button>
+          <button>About Us</button>
         </div>
       </Description>
       <Image>
-        <img
+        <motion.img
+          animate={{
+            opacity: 1,
+            transform: { translateX: 0 },
+            transition: { duration: 1 },
+            x: 0,
+          }}
+          initial={{ opacity: 0, x: 50 }}
           src={reading}
           alt="school library man 9-maktab pastdarg'om xoncharbog'"
         />
@@ -36,7 +75,5 @@ const AboutSection = () => {
     </About>
   );
 };
-
-
 
 export default AboutSection;
