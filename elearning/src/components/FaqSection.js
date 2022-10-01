0c0b0fe53e870beaff0d6701f4170689a21c44af
Javohirbek <javohirbekkhaydarov@gmail.com>
2022-10-02 00:09:08 +0500

    added AnimateSharedLayout layout"

--- elearning/src/components/FaqSection.js
+++ elearning/src/components/FaqSection.js
@@ -2,62 +2,59 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 const FaqSection = () => {
   return (
     <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <Toggle>
-        <div className="question">
-          <h4>Can I learn programming from 0 on your platform?</h4>
-
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
-              quidem.
-            </p>
+      <AnimateSharedLayout layout>
+        <Toggle title="Can I learn programming from 0 on your platform?">
+          <div className="question">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+                quidem.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="faq-line"></div>
-      </Toggle>
-
-      <div className="question">
-        <h4>How much do I have to pay per month?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
-            quidem.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-
-      <div className="question">
-        <h4>Is it possible to pay in full in one go?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
-            quidem.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-
-      <div className="question">
-        <h4>If I buy the course, can my brother and I also study?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
-            quidem.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+        </Toggle>
+        <Toggle title="How much do I have to pay per month?">
+          <div className="question">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+                quidem.
+              </p>
+            </div>
+          </div>
+        </Toggle>
+        <Toggle title="Is it possible to pay in full in one go?">
+          <div className="question">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+                quidem.
+              </p>
+            </div>
+          </div>
+        </Toggle>
+        <Toggle title="If I buy the course, can my brother and I also study?">
+          <div className="question">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+                quidem.
+              </p>
+            </div>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
@@ -67,11 +64,11 @@ const Faq = styled(About)`
   margin: 2rem;
 
   h2 {
-    padding-bottom: 2rem;
+    padding-bottom: 1rem;
     font-weight: lighter;
   }
   .question {
-    padding: 3rem 0rem;
+    padding: 1rem 0rem;
     cursor: pointer;
   }
   .answear {
@@ -84,6 +81,7 @@ const Faq = styled(About)`
     background-color: #ccc;
     height: 0.1rem;
     width: 100%;
+    margin: 1rem auto;
   }
 `;
 
