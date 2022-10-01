9afa01ef6fe479728a33319cd433fd5e354015eb
Javohirbek <javohirbekkhaydarov@gmail.com>
2022-10-02 00:19:31 +0500

    complete nav

--- elearning/src/components/Nav.js
+++ elearning/src/components/Nav.js
@@ -1,24 +1,42 @@
 import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 const Nav = () => {
+  const { pathname } = useLocation();
   return (
     <StyledNav>
       <h1>
         <Link id="logo" to="/">
           {" "}
-           We School
+          We School
         </Link>
       </h1>
       <ul>
         <li>
           <Link to="/">About US</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="courses"> Courses</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/courses" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="contact"> Contact Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/contact" ? "50%" : "0%" }}
+          />
         </li>
       </ul>
     </StyledNav>
@@ -50,6 +68,15 @@ const StyledNav = styled.nav`
     padding-left: 10rem;
     position: relative;
   }
+`;
+const Line = styled(motion.div)`
+  height: 0.2rem;
+  background-color: #fff;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  border-radius: 5px;
 `;
 
 export default Nav;
